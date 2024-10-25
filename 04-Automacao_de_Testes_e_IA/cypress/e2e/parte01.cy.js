daaf5765c1252b16d906bf4b2b56dcc8275e1e21
Anderson Corrêa <andersoncorrea8001@gmail.com>
2024-10-25 15:30:05 -0300

    Resolucao da primeira parte do desafio
    Finalizada a primeira parte do desafio sobre implementacao do Cypress com IA

---
+++ 04-Automacao_de_Testes_e_IA/cypress/e2e/parte01.cy.js
@@ -0,0 +1,36 @@
+describe('Navegação pela página', () => {
+  
+  beforeEach(() => {
+    cy.visit('https://automationexercise.com/')
+  })
+
+  
+  it('Busca de produtos', () => {
+    cy.pesquisarProduto('T-shirts')
+  })
+
+  it('Efetuar Login com credencias certas', () => {
+    cy.logarNaConta('usercypress@mail.com', 'cypress')
+    cy.get('#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a').should('contain', 'Logout');
+  })
+  
+
+  it('Efetuar Login com credencias incorretas', () => {
+    cy.logarNaConta('user@mail.com', 'user')
+    cy.url().should('include', '/login');
+    cy.get('#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > p').should('contain', 'incorrect');
+  })
+
+  it('Processo de checkout', () => {
+    cy.logarNaConta('usercypress@mail.com', 'cypress')
+    cy.pesquisarProduto('T-shirts')
+    cy.get('body > section > div > div > div.col-sm-9.padding-right > div.product-details > div.col-sm-7 > div > span > button').click()
+    cy.get('#cartModal > div > div > div.modal-body > p:nth-child(2) > a > u').click()
+    cy.get('#do_action > div.container > div > div > a').click()
+    cy.get('#ordermsg > textarea').type('Deixa o like Família!')
+    cy.get('#cart_items > div > div:nth-child(7) > a').click()
+    cy.preencherFormularioPagamento('Deixa', 'o', 'like', 'familia', '!')
+    cy.get('#submit').click()
+    cy.url().should('include', '/payment_done')
+  }) 
+})
---
+++ 04-Automacao_de_Testes_e_IA/cypress/support/commands.js
@@ -0,0 +1,32 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+Cypress.Commands.add('pesquisarProduto', (nomeProduto) => {
+    cy.get('#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(2) > a').click()
+    cy.get('#search_product').type(nomeProduto)
+    cy.get('#submit_search').click()
+    cy.get('body > section:nth-child(3) > div > div > div.col-sm-9.padding-right > div > div.col-sm-4 > div > div.choose > ul > li > a').click()
+})
+
+Cypress.Commands.add('logarNaConta', (email, senha) => {
+    cy.get('#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a').click()
+    cy.get('#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=email]:nth-child(2)').type(email)
+    cy.get('#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > input[type=password]:nth-child(3)').type(senha)
+    cy.get('#form > div > div > div.col-sm-4.col-sm-offset-1 > div > form > button').click()
+})
+
+Cypress.Commands.add('preencherFormularioPagamento', (nome, numero, cvc, mes, ano) => {
+    cy.get('#payment-form > div:nth-child(2) > div > input').type(nome);
+    cy.get('#payment-form > div:nth-child(3) > div > input').type(numero);
+    cy.get('#payment-form > div:nth-child(4) > div.col-sm-4.form-group.cvc > input').type(cvc);
+    cy.get('#payment-form > div:nth-child(4) > div:nth-child(2) > input').type(mes);
+    cy.get('#payment-form > div:nth-child(4) > div:nth-child(3) > input').type(ano);
+  });
+  
